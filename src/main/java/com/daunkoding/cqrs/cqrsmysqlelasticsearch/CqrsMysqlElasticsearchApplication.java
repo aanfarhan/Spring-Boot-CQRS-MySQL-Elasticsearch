2e38e9b8351565e3fac695cad32617ab24c16c89
Farhan <abaz.nurfarhana@gmail.com>
2020-05-01 13:56:28 +0700

    first commit

---
+++ src/main/java/com/daunkoding/cqrs/cqrsmysqlelasticsearch/CqrsMysqlElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.daunkoding.cqrs.cqrsmysqlelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsMysqlElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsMysqlElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daunkoding/cqrs/cqrsmysqlelasticsearch/CqrsMysqlElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daunkoding.cqrs.cqrsmysqlelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsMysqlElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
